0b1aefb41e2267e0f09734c564a002b5f3a8c35d
oKelvinCosta <okelvincosta@gmail.com>
2025-01-20 23:54:27 -0300

    Implement game logic for Tic-Tac-Toe with alternating player turns and winner calculation

--- src/APP.jsx
+++ src/APP.jsx
@@ -19,13 +19,25 @@ function Square({ value, onSquareClick }) {
 
 // Componente principal
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   // Instead, you’ll pass down a function from the Board component to the Square component, and you’ll have Square call that function when a square is clicked.
-  function handleClick(index) {
-    const newSquares = squares.slice();
-    newSquares[index] = "X";
-    setSquares(newSquares);
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+
+    const nextSquares = squares;
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
@@ -47,4 +59,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
